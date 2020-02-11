c263c323b67a0369c7fd815c74c6f124c640b74d
Nathan-Binkley <nbink2@gmail.com>
2020-02-11 15:27:20 -0500

    move towards stuff

--- owo.py
+++ owo.py
@@ -14,20 +14,30 @@ text=""
 # 
 
 
-filterList = ['://','www.','.com','.net','.gov','.org','https','http', '@', '#']
+filterList = ['://','www.','.com','.net','.gov','.org','https','http', '@', '#', 'RT']
+
+auth = tweepy.OAuthHandler(keys.API_KEY[0], keys.API_KEY[1])
+auth.set_access_token(keys.ACCESS_TOKEN[0],keys.ACCESS_TOKEN[1])
+word = tweepy.API(auth)
+people = ['realDonaldTrump', ]
+whom = 'realDonaldTrump'
+status = word.get_user("@"+whom)
+status = status._json
+
+text = status['status']
+text = text['text']
+tweet_id = status['id']
+twitter_at = status['screen_name']
 
 def owo(text):
     texts = text.split(" ")
     owod = ""
-    
-    
     for i in texts:
         
         temp = False
         for j in filterList: #allows filtering 
             if j in i:
                 owod += i
-                
                 temp = True
                 break
 
@@ -38,48 +48,50 @@ def owo(text):
                     owod += "w"
                 elif j == "L" or j == "R":
                     owod += "W"
-                # elif i == " ":
-                #     if random.randint(0,100) < 10:
-                #         helper = random.randint(0,60)
-                #         if helper < 10:
-                #             owod += " owo "
-                #         elif helper < 20 and helper >= 10:
-                #             owod += " XD "
-                #         elif helper < 30 and helper >= 20:
-                #             owod += " ouo "
-                #         elif helper < 40 and helper >= 30:
-                #             owod += " OwO *notices bulge* "
-                #         elif helper < 50 and helper >= 40:
-                #             owod += " rawr " 
-                #         else:
-                #             owod += " *nuzzles you* "
-                #     else:
-                #         owod += " "
+                elif i == " ":
+                    if random.randint(0,100) < 1:
+                        helper = random.randint(0,60)
+                        if helper < 10:
+                            owod += " owo "
+                        elif helper < 20 and helper >= 10:
+                            owod += " XD "
+                        elif helper < 30 and helper >= 20:
+                            owod += " ouo "
+                        elif helper < 40 and helper >= 30:
+                            owod += " OwO *notices bulge* "
+                        elif helper < 50 and helper >= 40:
+                            owod += " rawr " 
+                        else:
+                            owod += " *nuzzles you* "
+                    else:
+                        owod += " "
                 else:
                     owod += j
         owod += " "
     return owod
 
 
-auth = tweepy.OAuthHandler(keys.API_KEY[0], keys.API_KEY[1])
-auth.set_access_token(keys.ACCESS_TOKEN[0],keys.ACCESS_TOKEN[1])
-word = tweepy.API(auth)
-status = word.get_user("@realDonaldTrump")
-status = status._json
 
-text = status['status']
-text = text['text']
-tweet_id = status['id']
-print(tweet_id)
-print(text)
-print(owo(text))
+# print(text)
+# print(owo(text))
 
+tweet = ''
+# tweet = "@" + twitter_at + " " + owo(text)
+if whom in owo(text):
+    tweet = owo(text)
+else:
+    tweet = "@" + twitter_at + " " + owo(text)
 
 with open("Trumps.json", 'w') as f:
     json.dump(status, f, indent=4)
 
-word.update_status(status=owo(text), in_reply_to_status_id = tweet_id, auto_populate_reply_metadata=True)
-print("Tweeted:", owo(text))
+confirm = input("Would you like to tweet: " + tweet + " (y/n) ") 
+if confirm == "y" or confirm == "Y":
+    word.update_status(status=tweet, in_reply_to_status_id = tweet_id)
+    print("Tweeted:", tweet)
+else:
+    print("Not tweeted")
+
 
 # ----------------- READ IN FROM FILE IF NECESSARY --------------------- #
 # filename = "TestFile" #no ".txt" plz
