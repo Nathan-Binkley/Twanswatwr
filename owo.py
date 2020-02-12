a2a7821e4736b235be0f9e55e5ef0016cf25e60e
Nathan-Binkley <nbink2@gmail.com>
2020-02-12 09:12:06 -0500

    full tweet fix

--- owo.py
+++ owo.py
@@ -19,29 +19,30 @@ filterList = ['://','www.','.com','.net','.gov','.org','https','http', '@', '#',
 auth = tweepy.OAuthHandler(keys.API_KEY[0], keys.API_KEY[1])
 auth.set_access_token(keys.ACCESS_TOKEN[0],keys.ACCESS_TOKEN[1])
 word = tweepy.API(auth)
-people = ['realDonaldTrump', ]
+people = ['realDonaldTrump']
 whom = 'realDonaldTrump'
-status = word.get_user("@"+whom)
+status = word.user_timeline(id = whom , count = 1, tweet_mode='extended')[0]
 status = status._json
 
-text = status['status']
-text = text['text']
+with open("Trumps.json", 'w') as f:
+    json.dump(status, f, indent=4)
+
+text = status['full_text']
+# text = text['text']
 tweet_id = status['id']
-twitter_at = status['screen_name']
+twitter_at = status['user']['screen_name']
+print(twitter_at)
 
 def owo(text):
     texts = text.split(" ")
     owod = ""
     for i in texts:
-        
         temp = False
         for j in filterList: #allows filtering 
             if j in i:
                 owod += i
                 temp = True
                 break
-
-        
         if not temp:
             for j in i:
                 if j == "l" or j == "r":
@@ -49,7 +50,7 @@ def owo(text):
                 elif j == "L" or j == "R":
                     owod += "W"
                 elif i == " ":
-                    if random.randint(0,100) < 1:
+                    if random.randint(0,100) < 10:
                         helper = random.randint(0,60)
                         if helper < 10:
                             owod += " owo "
@@ -82,8 +83,7 @@ if whom in owo(text):
 else:
     tweet = "@" + twitter_at + " " + owo(text)
 
-with open("Trumps.json", 'w') as f:
-    json.dump(status, f, indent=4)
+
 
 confirm = input("Would you like to tweet: " + tweet + " (y/n) ") 
 if confirm == "y" or confirm == "Y":
