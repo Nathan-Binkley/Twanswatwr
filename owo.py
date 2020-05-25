f3d47f94187589af797fa89acebc9e9eeee7c172
Nathan-Binkley <nbink2@gmail.com>
2020-05-24 23:27:05 -0400

    AWS Testing

--- owo.py
+++ owo.py
@@ -15,7 +15,7 @@ auth = tweepy.OAuthHandler(keys.API_KEY[0], keys.API_KEY[1])
 auth.set_access_token(keys.ACCESS_TOKEN[0],keys.ACCESS_TOKEN[1])
 word = tweepy.API(auth)
 
-people_at = ['realDonaldTrump', 'JoeBiden', 'LindseyGrahamSC']
+people_at = ['realDonaldTrump', 'JoeBiden', 'LindseyGrahamSC','NathanBinkley1']
 people_id = []
 person = ['25073877']
 
